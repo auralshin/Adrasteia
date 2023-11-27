43891e6129e6579952f7e5d67e6d75f407ab5afa
auralshin <anshulspartan141@gmail.com>
2023-11-27 22:56:04 +0530

    j

---
+++ packages/web/src/app/swap/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import CryptoSwap from "./components/swap";
+export default function SwapPage() {
+    return (
+        <>
+        <div className="hidden flex-col md:flex">
+            <CryptoSwap />
+        </div>
+        </>
+    );
+    }
